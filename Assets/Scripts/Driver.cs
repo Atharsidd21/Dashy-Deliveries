368da30b57b628afc8ca77465a4c37adf8d5220d
Athar Siddiqui <atharsiddiqui211995@gmail.com>
2025-11-13 20:51:35 +0530

    Day3

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -8,38 +8,58 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        //Method1: Using the old Input System
-        /* float move = Input.GetAxis("Vertical") *  Movespeed;
+        /*/Method1: Using the old Input System
+         float move = Input.GetAxis("Vertical") *  Movespeed;
          float steer = Input.GetAxis("Horizontal")  * Rotationspeed;
 
          transform.Rotate(0, 0f, steer);
          transform.Translate(0, move , 0);/*/
 
-        //Method2: Using the new Input System
+        //Method2: Using the new Input System//
         float move = 0f;
         float steer= 0f;
         if (Keyboard.current.wKey.isPressed)
         {
             move = 1f;
-            Debug.Log("Moving Forward");
+            //Debug.Log("Moving Forward");
         }
         else if (Keyboard.current.sKey.isPressed)
         {
             move = -1f;
-            Debug.Log("Moving Backward");
+            //Debug.Log("Moving Backward");
         }
         if (Keyboard.current.aKey.isPressed)
         {
             steer = 1f;
-            Debug.Log("Turning Left");
+            //Debug.Log("Turning Left");
         }
         else if (Keyboard.current.dKey.isPressed)
         {
             steer = -1f;
-            Debug.Log("Turning Right");
+            //Debug.Log("Turning Right");
         }
-        transform.Rotate(0, 0f, steer * Rotationspeed);
-        transform.Translate(0, move * Movespeed, 0);
+        //calculate movement and steering amounts
+        float moveAmount = move * Movespeed * Time.deltaTime;
+        float steerAmount = steer * Rotationspeed * Time.deltaTime;
+        transform.Rotate(0, 0f, steerAmount);
+        transform.Translate(0, moveAmount, 0);
 
     }
+    /*This Part can be done by creating a seprate script 
+     but I am doing this here for my comfort,
+    but making a seperate script is recommended.*/
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided");
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered");
+    }
+    /*Optional Just for Practice
+    void OnTriggerExit2D(Collider2D collision) 
+    {
+        Debug.Log("Trigger Exited");
+    }*/
+     
 }
